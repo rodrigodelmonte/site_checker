7d931e3652b53cffaa70bb163882233286feffe5
rodrigodelmonte <rodrigo.monte@onefootball.com>
2021-06-07 16:49:14 +0200

    Add http_client used by the producer worker

---
+++ site_checker/clients/http_client.py
@@ -0,0 +1,28 @@
+import logging
+
+import requests
+from requests.adapters import HTTPAdapter
+from requests.exceptions import ConnectionError
+from requests.models import Response
+from requests.packages.urllib3.util.retry import Retry
+
+logger = logging.getLogger(__name__)
+
+
+def get_response(url: str) -> Response:
+
+    # https://findwork.dev/blog/advanced-usage-python-requests-timeouts-retries-hooks/#retry-on-failure
+    session = requests.Session()
+    retry_strategy = Retry(
+        total=3, backoff_factor=1, status_forcelist=[429, 500, 502, 503, 504]
+    )
+    adapter = HTTPAdapter(max_retries=retry_strategy)
+    session.mount("https://", adapter)
+    session.mount("http://", adapter)
+
+    try:
+        response = session.get(url, timeout=(2, 5))
+    except ConnectionError as e:
+        logger.exception(f"HTTP Connection Error occurred:\n{e}")
+
+    return response
